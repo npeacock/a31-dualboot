d45ed47c73bf04aaf133b14d5fd463a757c7db28
Neal Peacock <neal@pengpod.com>
2013-08-23 02:39:33 -0400

    dualboot: add commands to lookup and store default boot option
    Change-Id: Iaec92bfac8ec03d8417104a4bbfa7e2d7511d1e6

--- specialreboot.c
+++ specialreboot.c
@@ -5,9 +5,7 @@
  *      Author: Neal Peacock
  */
 
-// I'm not going to bother and try and included these from the right spots, this is a universal thing
-#define  LINUX_REBOOT_CMD_RESTART 0x01234567
-#define RB_AUTOBOOT LINUX_REBOOT_CMD_RESTART
+#include "specialreboot.h"
 
 int special_reboot( char *arg )
 {
@@ -20,5 +18,103 @@ int special_reboot( char *arg )
 	return ret;
 }
 
+int mount_nanda()
+{
+	int ret = 0;
 
+	if( ! access(DEF_BOOT_FILE, F_OK) ){
+		const char* devname = NANDA_BLK_DEV;
+		const char* mount_point = NANDA_MNT_POINT;
+		const char* filesystem = "vfat";
+		const unsigned long flags = MS_NOATIME | MS_NODIRATIME;
+
+		ret = mount(devname, mount_point, filesystem, flags, NULL);
+	}
+
+	return ret;
+
+}
+
+int unmount_nanda()
+{
+	return umount(NANDA_MNT_POINT);
+}
+
+char* lookup_default_reboot()
+{
+	char* ret = DEF_BOOT_STRING;
+	int mounterr = 0;
+
+	mounterr = mount_nanda();
+
+	if(!mounterr){
+		FILE* defboot = fopen(DEF_BOOT_FILE, "r");
+		if (defboot == NULL) {
+			LOGE("failed to open %s (%s)\n", DEF_BOOT_FILE,strerror(errno));
+			return DEF_BOOT_STRING;
+		}
+
+		char buffer[1024];
+		int i;
+		while (fgets(buffer, sizeof(buffer)-1, defboot)) {
+			for (i = 0; buffer[i] && isspace(buffer[i]); ++i);
+			if (buffer[i] == '\0' || buffer[i] == '#') continue;
+
+			char* def_boot_type = strtok(buffer+i, " \t\n");
+			ret = strdup(def_boot_type);
+		}
+
+		fclose(defboot);
+		unmount_nanda();
+	}
+
+	return ret;
+}
+
+
+int set_default_reboot( char* defbootstr)
+{
+	int ret = 0;
+	int test = 0;
+
+	ret = mount_nanda();
+
+	if(!ret){
+		FILE* defboot = fopen(DEF_BOOT_FILE, "w");
+		if (defboot == NULL) {
+			LOGE("failed to open %s (%s)\n", DEF_BOOT_FILE,strerror(errno));
+			ret = errno;
+		}else{
+			test = fputs(defbootstr,defboot);
+
+			if( test == EOF){
+				LOGE("failed to write to %s default boot of %s)\n", DEF_BOOT_FILE, defbootstr);
+				ret = test;
+			}else{
+				ret = fclose(defboot);
+
+				if(ret != 0){
+					LOGE("failed to close %s\n", DEF_BOOT_FILE);
+				}else {
+					ret = unmount_nanda();
+
+					if(ret != 0){
+						LOGE("failed to unmount nanda\n");
+					}
+				}
+			}
+		}
+	}
+	return ret;
+}
+
+
+
+int special_reboot_default( )
+{
+
+	char* type = lookup_default_reboot();
+
+	special_reboot(type);
+}
 
---
+++ specialreboot.h
@@ -0,0 +1,33 @@
+/*
+ * specialreboot.c
+ *
+ *  Created on: Aug 8, 2013
+ *      Author: Neal Peacock
+ */
+#ifndef _SPECIAL_REBOOT_H
+#define _SPECIAL_REBOOT_H	1
+
+#include <sys/mount.h>  // for _IOW, _IOR, mount()
+#include <unistd.h>
+#include <errno.h>
+#include <libio.h>
+
+// I'm not going to bother and try and included these from the right spots, this is a universal thing
+#define  LINUX_REBOOT_CMD_RESTART 0x01234567
+#define RB_AUTOBOOT LINUX_REBOOT_CMD_RESTART
+
+#define NANDA_BLK_DEV "/dev/block/nanda"
+#define NANDA_MNT_POINT "/mnt"
+#define DEF_BOOT_FILE "/mnt/default-boot.conf"
+
+#define LINUX_BOOT_STRING "linux"
+#define AND_BOOT_STRING "android"
+#define DEF_BOOT_STRING "default"
+
+int special_reboot( char *arg );
+int mount_nanda();
+char* lookup_default_reboot();
+int special_reboot_default( );
+
+#endif
+
